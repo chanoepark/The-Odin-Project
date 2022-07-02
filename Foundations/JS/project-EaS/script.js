f4cfbdedaec5765521c2f69b8f3ed8860fa8ac41
Chanoe Andrew Park <chanoepark@gmail.com>
2022-07-02 14:28:49 -0400

    Completed step 4 (adjust grid size)

--- Foundations/JS/project-EaS/script.js
+++ Foundations/JS/project-EaS/script.js
@@ -1,19 +1,44 @@
-const GRIDSIZE = 16;
+const DEFAULTGRIDSIZE = 16;
 
-container = document.querySelector('.container');
+function createGrid(gridSize = DEFAULTGRIDSIZE) {
+    const container = document.querySelector('.container');
 
-for (let row = 0; row < GRIDSIZE; row++) {
-    const gridRow = document.createElement('div');
-    gridRow.classList.add('grid-row');
+    for (let row = 0; row < gridSize; row++) {
+        const gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
 
-    for (let col = 0; col < GRIDSIZE; col++) {
-        const gridSquare = document.createElement('div');
-        gridSquare.classList.add('grid-square');
-        gridSquare.addEventListener('mouseover', e => {
-            e.target.style.backgroundColor = 'red';
-        });
-        gridRow.appendChild(gridSquare);
+        for (let col = 0; col < gridSize; col++) {
+            const gridSquare = document.createElement('div');
+            gridSquare.classList.add('grid-square');
+            gridSquare.addEventListener('mouseover', e => {
+                e.target.style.backgroundColor = 'red';
+            });
+            gridRow.appendChild(gridSquare);
+        }
+
+        container.appendChild(gridRow);
     }
-
-    container.appendChild(gridRow);
 }
+
+function removeGrid() {
+    const container = document.querySelector('.container');
+
+    while (container.hasChildNodes())
+        container.removeChild(container.firstChild);
+}
+
+function adjustGridSize() {
+    removeGrid();
+    
+    let gridSize = DEFAULTGRIDSIZE;
+
+    do {
+        gridSize = Number(prompt("Desired number of squares in grid (1-100):"));
+    } while(gridSize < 1 || gridSize > 100 || !Number.isInteger(gridSize));
+
+    createGrid(gridSize);
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', adjustGridSize);
+createGrid();
